d231259e88cb4df3cb75ff2d31f6ba0ec6a2da1c
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2025-04-07 19:10:05 -0500

    Tested balanceOf for StrategyAave.sol and added Errors.sol file

---
+++ hardhat/contracts/libraries/Errors.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library Errors {
+  error InvalidAddress();
+  error TokenSupportUnchanged();
+  error PoolSupportUnchanged();
+  error NoPoolForToken();
+  error InvalidAmount();
+  error UnsupportedToken();
+  error UnsupportedTokenForPool();
+  error InsufficientWithdrawal();
+}
